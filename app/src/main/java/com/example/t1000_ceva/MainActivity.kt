01f97fefb5a5d730ecab3209f36c5f2445a739cf
Emmanuel.Ebhomenye <eebhomenye@yahoo.com>
2022-04-16 18:07:29 +0100

    Added Hilt annotation

--- app/src/main/java/com/example/t1000_ceva/MainActivity.kt
+++ app/src/main/java/com/example/t1000_ceva/MainActivity.kt
@@ -7,7 +7,9 @@ import android.util.Log
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.t1000_ceva.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
